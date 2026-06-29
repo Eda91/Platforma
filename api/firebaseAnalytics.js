b8f8348cebea92aff4f8fd6796d8e171c2ff0157
Eda91 <eda.eltari@gmail.com>
2026-06-29 09:57:16 +0200

    Fixing the problem  with mobile now1

--- api/firebaseAnalytics.js
+++ api/firebaseAnalytics.js
@@ -1,14 +1,14 @@
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, increment } from "firebase/firestore";
 import { db } from "../src/firebase";
 
 export async function uploadStats(stats) {
   try {
     await setDoc(
       doc(db, "clicks", "3bFrd9Iw5HtgL6tMR6YO"),
-      {
-        clicks: stats,
-        updatedAt: new Date().toISOString(),
-      },
+    {
+      clicks: increment(1),
+      updatedAt: new Date().toISOString(),
+    },
       { merge: true }
     );
   } catch (err) {
