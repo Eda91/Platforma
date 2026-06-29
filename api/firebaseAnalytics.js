1af3e9e7ddb02e9928dbca92e4b9946ba00dea5c
Eda91 <eda.eltari@gmail.com>
2026-06-29 09:49:33 +0200

    Fixing the problem  with mobile now

--- api/firebaseAnalytics.js
+++ api/firebaseAnalytics.js
@@ -1,18 +1,22 @@
-import { doc, setDoc, increment } from "firebase/firestore";
+import { doc, setDoc } from "firebase/firestore";
 import { db } from "../src/firebase";
 
-export async function uploadStats() {
+export async function uploadStats(stats) {
   try {
     await setDoc(
       doc(db, "clicks", "3bFrd9Iw5HtgL6tMR6YO"),
       {
-        clicks: increment(1),
+        clicks: stats,
         updatedAt: new Date().toISOString(),
       },
       { merge: true }
     );
   } catch (err) {
-    alert(err.code + "\n\n" + err.message);
-    console.error(err);
-  }
+  alert(
+    err.code +
+    "\n\n" +
+    err.message
+  );
+  console.error(err);
+}
 }
